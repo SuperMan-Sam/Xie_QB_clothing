40acf21fffee8f8c731032fe3568d5bb2841b41b
SuperMan-Sam <31952622+SuperMan-Sam@users.noreply.github.com>
2023-11-15 01:18:06 -0500

    2

---
+++ [Clothings]/[v8]/1370 - V8 Clothing Female/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_1370 - v8 clothing female.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_1370 - v8 clothing female.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
